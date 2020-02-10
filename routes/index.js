c6e2b239792c982f250697dff3fde8667bedc2e3
goat <giovanni.greco@quinnipiac.edu>
2020-02-09 21:40:26 -0500

    Fixed it

--- routes/index.js
+++ routes/index.js
@@ -7,8 +7,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Giovanni Grecos Page' });
 });
 router.get('/api/add', function(req, res, next) {
-var n1 = req.query.n1;
-var n2 = req.query.n2;
+var n1 = parseInt(req.query.n1);
+var n2 = parseInt(req.query.n2);
 res.json(math.add(n1,n2));
 });
 module.exports = router;
