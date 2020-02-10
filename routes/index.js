45e9fbad062054bd082189b527a731d00398a759
goat <giovanni.greco@quinnipiac.edu>
2020-02-09 21:29:18 -0500

    Hooking up heroku to gulp and mocha

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
+var math = require('./math');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Giovanni Grecos Page' });
 });
-
+router.get('/api/add', function(req, res, next) {
+var n1 = parseInt(req.query.n1);
+var n2 = parseInt(req.query.n2);
+res.json(math.add(n1,n2));
+});
 module.exports = router;
