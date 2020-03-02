4de63e581d3ce0f43d6cc2cebbabc920c6173e8f
goat <giovanni.greco@quinnipiac.edu>
2020-03-01 20:02:41 -0500

    Added custom logger

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var math = require('./math');
-
+var logger = require('../logger');
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Giovanni Grecos Page' });
@@ -9,6 +9,7 @@ router.get('/', function(req, res, next) {
 router.get('/api/add', function(req, res, next) {
 var n1 = parseInt(req.query.n1);
 var n2 = parseInt(req.query.n2);
+logger.info('/api/add used to add '+ n1 + ' and ' + n2);
 res.json(math.add(n1,n2));
 });
 module.exports = router;
